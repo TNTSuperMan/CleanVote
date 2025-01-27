e6ec849de4c01824adafcfe46a6f2a84b2d0eeca
TNTSuperMan <tntsuperga@gmail.com>
2025-01-27 16:24:53 +0900

    統合(?)

---
+++ scripts/dev.ts
@@ -0,0 +1,16 @@
+import { createServer } from "vite";
+import app from "../server/src/index";
+import { serve } from "bun";
+
+if(process.argv.length == 2){
+    createServer({
+        configFile: "./client/vite.config.ts",
+        root: "./client"
+    }).then(e=>e.listen(4000)).then(e=>{
+        console.log("client started")
+    })
+    serve({
+        fetch: app.fetch,
+        port: 3928
+    });
+}
