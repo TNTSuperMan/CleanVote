2b13a742a83832e5efb43ae8268ba9c9b8cf6ea1
TNTSuperMan <tntsuperga@gmail.com>
2025-01-27 16:37:46 +0900

    envにAPIKEYを追加

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
     readonly VITE_TURNSTILE_KEY: string;
+    readonly VITE_API_KEY: string;
 }
 interface ImportMeta {
     readonly env: ImportMetaEnv;
