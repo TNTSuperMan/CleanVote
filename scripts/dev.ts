b718d057515b7fc26a21c45e47be7e7e0749eccd
TNTSuperMan <tntsuperga@gmail.com>
2025-01-27 16:33:51 +0900

    コマンド修正・APIURLの

--- scripts/dev.ts
+++ scripts/dev.ts
@@ -7,7 +7,7 @@ if(process.argv.length == 2){
         configFile: "./client/vite.config.ts",
         root: "./client"
     }).then(e=>e.listen(4000)).then(e=>{
-        console.log("client started")
+        console.log("client started at http://localhost:4000");
     })
     serve({
         fetch: app.fetch,
