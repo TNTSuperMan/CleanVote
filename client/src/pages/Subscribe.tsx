07f14b58c1e0a5852972c200e7b82950c3caee24
TNTSuperMan <tntsuperga@gmail.com>
2025-01-28 23:22:23 +0900

    戻るボタン

--- client/src/pages/Subscribe.tsx
+++ client/src/pages/Subscribe.tsx
@@ -1,6 +1,7 @@
 import './Subscribe.scss';
 import { useEffect, useRef, useState } from "react";
 import { UncoolTurnstile } from "../components/Turnstile";
+import { Link } from 'react-router-dom';
 
 export const Subscribe = () => {
     const [token, setToken] = useState<string|undefined>();
@@ -38,6 +39,7 @@ export const Subscribe = () => {
 
     return <div className="subscribe">
         <h1>登録フォーム</h1>
+        <Link to="/">戻る</Link><br/>
         {error ? <div className="err"></div> : <></>}
         タイトル(長さ：{titlelen.toString()}b / 256b)：<input type="text" value={title} onChange={e=>setTitle(e.target.value)} /><br></br>
         説明(長さ：{desclen.toString()}b / 1024b)<textarea value={description} onChange={e=>setDescription(e.target.value)} placeholder="説明を入力" />
