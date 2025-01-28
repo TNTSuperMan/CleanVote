ea93e5b1029fe3786e822a9201d185c9e7decfa8
TNTSuperMan <tntsuperga@gmail.com>
2025-01-29 00:32:05 +0900

    メッセージを日本語化

--- server/src/subscribe.ts
+++ server/src/subscribe.ts
@@ -11,14 +11,14 @@ app.use("/subscribe", cors({
 app.post('/subscribe', c =>
   c.req.json<{token: string, title: string, description: string}>().then(body=>{
     if(typeof body !== "object" || typeof body.token !== "string" || typeof body.title !== "string" || typeof body.description !== "string"){
-      throw new HTTPException(400, { message: "Invalid JSON props" });
+      throw new HTTPException(400, { message: "無効なJSON形式" });
     }else{
       if(encoder.encode(body.title).length > 256)
         return new Promise<Response>(res=>
-          res(c.text("Too long title",400)));
+          res(c.json({message:"タイトルが長すぎます"},400)));
       if(encoder.encode(body.description).length > 1024)
         return new Promise<Response>(res=>
-          res(c.text("Too long description",400)));
+          res(c.json({message:"説明が長すぎます"},400)));
       return fetch("https://challenges.cloudflare.com/turnstile/v0/siteverify",{
         body: JSON.stringify({
           secret: c.env.TURNSTILE_SECRET_KEY,
@@ -30,14 +30,14 @@ app.post('/subscribe', c =>
         method: "POST"
       }).then<{success: boolean}>(e=>e.json())
       .then(e=>{
-        if(e.success){
+        if(e.success){//TODO:管理トークン等
           return c.text("You are human!")
         }else{
-          return c.text("YOU ARE ROBOT",400)
+          return c.json({message:"あなたはロボットです。"},400)
         }
       })
     }
   }).catch(()=>{
-    throw new HTTPException(400, { message: "Invalid JSON" });
+    throw new HTTPException(400, { message: "無効なJSON" });
   })
 )
