f0603db83dc95cba138cf571e7eeb464129b1f26
TNTSuperMan <tntsuperga@gmail.com>
2025-01-29 15:31:54 +0900

    環境変数を追加

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,10 @@
 import { Hono } from "hono";
 
-type Env = { TURNSTILE_SECRET_KEY: string };
+type Env = {
+    TURNSTILE_SECRET_KEY: string;
+    ACCOUNT_ID: string;
+    DB_ID: string;
+    TOKEN: string;
+    EMAIL: string;
+};
 export const app = new Hono<{ Bindings: Env }>()
