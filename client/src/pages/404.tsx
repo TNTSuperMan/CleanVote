b45f77051072e1140f1f0bd2732305360c14d97f
TNTSuperMan <tntsuperga@gmail.com>
2025-02-08 18:57:55 +0900

    誤解を招く表記を訂正

--- client/src/pages/404.tsx
+++ client/src/pages/404.tsx
@@ -3,6 +3,6 @@ import "./404.scss"
 export const NotFound = () => {
     return <div className="notfound">
         <h2>404 Not Found</h2>
-        <small>しかし実際のHTTPステータス上では200であり、これはReact Router上で存在しないルートに対するHTTPステータスとは無関係で意図的なメッセージです。メタすぎますか？</small>
+        <small>これはReact Router上で存在しないルートに対する意図的なメッセージであり、HTTPレスポンスステータスコードとは無関係で意図的なメッセージであり、実際のHTTPレスポンスステータスコードは200です。メタすぎますか？</small>
     </div>
 }
