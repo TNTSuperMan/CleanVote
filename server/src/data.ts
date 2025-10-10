d3711bcb09253a3783d0e1597684e4317cf376a1
TNTSuperMan <tnt-super-man@outlook.com>
2025-10-10 18:09:05 +0900

    dataのbindのとこ修正

--- server/src/data.ts
+++ server/src/data.ts
@@ -8,11 +8,12 @@ app.post("/data", async c => {
   
   const rawdata = await c.env.DB
     .prepare('SELECT title, description, options FROM ballot_boxes WHERE token = ?')
+    .bind(token)
     .first<{
       titile: string;
       description: string;
       options: string;
-    }>(token);
+    }>();
 
   if(!rawdata){
     throw new HTTPException(404, { message: "この投票先は見つかりませんでした。" });
